8047be4d52f3cad736cae2562747378473eb4227
이강준 <admin@gangjun.dev>
2020-05-01 15:54:43 +0900

    basic layout

---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav
+    class="flex items-center justify-between flex-wrap shadow-2xl paddingNav bg-gray-800"
+  >
+    <div class="flex items-center flex-shrink-0 mr-6 text-white">
+      <span class="font-semibold text-xl tracking-tight">Shol.xyz</span>
+    </div>
+  </nav>
+</template>
+
+<style>
+.paddingNav {
+  padding-left: 20vw;
+  padding-right: 20vw;
+  padding-top: 10px;
+}
+
+@media (max-width: 756px) {
+  .paddingNav {
+    padding-left: 30px;
+    padding-right: 30px;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <header class="bg-gray-800 text-white">
+    <div class="pt-16 text-lg">
+      쓸데없이 긴 URL 대신 짧은 URL 을 사용하세요!
+    </div>
+    <div class="flex justify-center pt-4">
+      <input
+        class="appearance-none block w-1/3 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+        type="text"
+        placeholder="긴 URL을 넣어주세요"
+        v-model="longurl"
+      />
+      <button
+        class="ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        type="button"
+        @click="getShort"
+      >
+        만들기
+      </button>
+    </div>
+  </header>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data: () => ({
+    longurl: '',
+    shorturl: ''
+  }),
+  methods: {
+    getShort() {}
   }
 }
 </script>
+
+<style>
+header {
+  height: 40vh;
+}
+</style>
